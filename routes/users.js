63a3450e6d7f4052e8c5ba4b982aa4925ca25098
Николай <kolebas1986@gmail.com>
2022-09-13 15:22:06 +0300

    fix: Удалить поле 'about' и добавить вместо него поле 'email'

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get('/me', getMe);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30),
   }),
 }), updateUserProfileById);
 
